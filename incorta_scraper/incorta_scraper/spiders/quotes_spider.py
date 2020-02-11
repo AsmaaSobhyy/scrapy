1ca003f2cc3056b1c3baa7b9950810ca37683db9
asmaa sobhy <asmaasobhy1995@gmail.com>
2020-02-11 22:18:51 +0200

    initial commit

---
+++ incorta_scraper/incorta_scraper/spiders/quotes_spider.py
@@ -0,0 +1,61 @@
+import scrapy
+from bs4 import BeautifulSoup
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "incorta"
+    start_urls = [
+        'https://docs.incorta.com/',
+    ]
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'incorta-%s.html' % page
+        txt=response.body
+        self.gettext(txt)
+
+        for href in response.css('div ul li div a::attr(href)'):
+            yield response.follow(href, callback=self.parse)
+
+
+
+    def gettext(self,html_doc):
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        txt=soup.get_text()
+        filename = 'incorta.txt'
+        with open(filename, 'a+') as f:
+            f.write(txt)
+
+
+
+
+
+
+
+
+
+
+        # next_page = response.css('a::attr(href)').get()
+        # if next_page is not None:
+        #     next_page = response.urljoin(next_page)
+        #     yield scrapy.Request(next_page, callback=self.parse)
+
+
+        # for quote in response.css('div.quote'):
+        #     yield {
+        #         'text': quote.css('span.text::text').get(),
+        #         'author': quote.css('small.author::text').get(),
+        #         'tags': quote.css('div.tags a.tag::text').getall(),
+        #     }
+
+        # next_page = response.css('li.next a::attr(href)').get()
+        # if next_page is not None:
+        #     next_page = response.urljoin(next_page)
+        #     yield scrapy.Request(next_page, callback=self.parse)
+
+
+    # def parse(self, response):
+    #     page = response.url.split("/")[-2]
+    #     filename = 'quotes-%s.html' % page
+    #     with open(filename, 'wb') as f:
+    #     f.write(response.body)
